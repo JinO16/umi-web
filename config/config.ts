360cd2607a94c19249400c2c99672d95bc8f7dac
Farben <farben@FarbendeMacBook-Pro.local>
2023-06-13 13:37:29 +0800

    配置构建目录

--- config/config.ts
+++ config/config.ts
@@ -2,17 +2,50 @@ import routes from "../src/router";
 import { defineConfig } from "umi";
 
 const isProduction = (env: string | undefined) => {
-  const envs = ['test', 'production'];
+  const envs = ['test', 'prod'];
   return env && envs.includes(env);
 }
 
+const baseUrl = 'ekpsj16/portal/new-portal-test';
 export default defineConfig({
   routes: routes,
   npmClient: 'pnpm',
   base: '/',
-  publicPath: isProduction(process.env.NODE_ENV) ? '/ekpsj16/portal/new-portal-test/' : '/',
+  publicPath: isProduction(process.env.UMI_ENV) ? `/${baseUrl}/` : '/',
   history: {
     type: 'hash'
   },
-  hash: true
+  hash: true,
+  // headScripts: [
+  //   { src: '/js/umi.js', defer: true },
+  // ],
+  // scripts: [
+  //   {
+  //     src: isProduction(process.env.UMI_ENV) ? `/${baseUrl}/js/umi.js` : '/js/umi.js'
+  //   }
+  // ],
+  chainWebpack: (config, { webpack }) => {
+    // 将js文件打包到dist/js文件中
+    config.output.filename('js/[name].[hash:8].js');
+    config.output.chunkFilename('js/[name].[hash:8]..async.js');
+
+    // 将css文件打包到dist/css文件中
+    config.plugin('mini-css-extract-plugin').tap((args) => [
+      {
+        ...args[0],
+        filename: 'css/[name].[hash:8]..css',
+        chunkFilename: 'css/[name].[hash:8]..chunk.css'
+      }
+    ])
+    // 更改生成的html模板配置
+    // config.plugin('html').tap(args => {
+    //   args[0].template = './public/index.html'; // 指定 HTML 模板文件
+    //   // args[0].scriptLoading = 'defer'; // 将所有 script 标签的 loading 属性设置为 defer
+    //   return args;
+    // })
+  },
+  // 配置全局变量，透传
+  define: {
+    "process.env.UMI_ENV": process.env.UMI_ENV
+  }
 });
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,20 +5,25 @@ import styles from './index.less';
 // 全局布局，默认会在所有的路由下生效
 export default function Layout(props: any) {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      {/* 可以配置全局导航栏 */}
+    <>
+      <div className={styles.navs}>
+        <h1>华夏基金智能工作平台</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/docs">Docs</Link>
+          </li>
+          <li>
+            <a href="https://github.com/umijs/umi">Github</a>
+          </li>
+        </ul>
+        <input type="text" placeholder='请输入搜索内容'/>
+        {/* 可以配置全局导航栏 */}
+      </div>
       <Outlet />
-    </div>
+    </>
+    
   );
 }
